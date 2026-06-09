d4b9b7b8a8fd328b017fd2cd9dd62dc9ceda7135
Drew <signaturesoundlighting@gmail.com>
2026-06-09 15:53:23 -0400

    add locations

--- js/locations-map.js
+++ js/locations-map.js
@@ -5,25 +5,28 @@
   if (!mapEl || typeof L === 'undefined') return;
 
   var wilmington = [34.2257, -77.9447];
-  var radiusMeters = 160934; // 100 miles
 
   var cities = [
     { name: 'Wilmington, NC', coords: [34.2257, -77.9447] },
     { name: 'Leland, NC', coords: [34.2563, -78.0447] },
-    { name: 'Burgaw, NC', coords: [34.5521, -77.9261] },
     { name: 'Castle Hayne, NC', coords: [34.3568, -77.8997] },
-    { name: 'Hampstead, NC', coords: [34.3677, -77.7105] },
-    { name: 'Surf City, NC (Topsail)', coords: [34.4332, -77.5436] },
-    { name: 'Oak Island, NC', coords: [33.9563, -78.1611] },
     { name: 'Carolina Beach, NC', coords: [34.0352, -77.8936] },
     { name: 'Wrightsville Beach, NC', coords: [34.2085, -77.7961] },
+    { name: 'Kure Beach, NC', coords: [33.9985, -77.9073] },
+    { name: 'Hampstead, NC', coords: [34.3677, -77.7105] },
+    { name: 'Holly Ridge, NC', coords: [34.4954, -77.5541] },
+    { name: 'Sneads Ferry, NC', coords: [34.5527, -77.3972] },
+    { name: 'Jacksonville, NC', coords: [34.7540, -77.4302] },
+    { name: 'Hubert, NC', coords: [34.6899, -77.2208] },
+    { name: 'Oak Island, NC', coords: [33.9563, -78.1611] },
     { name: 'Southport, NC', coords: [33.9204, -78.0203] },
     { name: 'Shallotte, NC', coords: [33.9732, -78.3858] },
-    { name: 'Jacksonville, NC', coords: [34.7540, -77.4302] },
-    { name: 'New Bern, NC', coords: [35.1085, -77.0441] },
-    { name: 'Kinston, NC', coords: [35.2627, -77.5816] },
-    { name: 'Fayetteville, NC', coords: [35.0527, -78.8784] },
-    { name: 'Myrtle Beach, SC', coords: [33.6891, -78.8867] }
+    { name: 'Calabash, NC', coords: [33.8907, -78.5683] },
+    { name: 'Ocean Isle Beach, NC', coords: [33.8943, -78.4267] },
+    { name: 'Bolivia, NC', coords: [34.0677, -78.1483] },
+    { name: 'Whiteville, NC', coords: [34.3388, -78.7031] },
+    { name: 'Little River, SC', coords: [33.8732, -78.6142] },
+    { name: 'North Myrtle Beach, SC', coords: [33.8160, -78.6800] }
   ];
 
   var map = L.map('service-map').setView(wilmington, 8);
@@ -32,14 +35,6 @@
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18
   }).addTo(map);
-
-  L.circle(wilmington, {
-    radius: radiusMeters,
-    color: '#9ACD32',
-    fillColor: '#9ACD32',
-    fillOpacity: 0.12,
-    weight: 2
-  }).addTo(map).bindPopup('<strong>100-Mile Service Radius</strong><br>Centered on Wilmington, NC');
 
   var markerIcon = L.divIcon({
     className: 'map-marker',
